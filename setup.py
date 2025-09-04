b0ccd8dfdd375a152806948f8094f51c5e3a4d79
Cody Hou <cody.houx@gmail.com>
2025-09-04 14:40:42 +1000

    Updated setup packages

--- setup.py
+++ setup.py
@@ -2,13 +2,25 @@ from setuptools import find_packages, setup
 
 setup(
     name="bigstar-data-migration",
+    version="0.1.0",
+    description="BigStar data migration orchestrated by Dagster + dbt + Airbyte",
     packages=find_packages(),
+    python_requires=">=3.9,<3.12",
     install_requires=[
-        "dbt-bigquery",
-        "dagster",
-        "dagster-cloud",
-        "dagster-dbt",
-        "dagster-airbyte",
+        "dbt-redshift>=1.7,<1.9",
+        "dagster>=1.6,<1.8",
+        "dagster-dbt>=0.23,<0.25",
+        "dagster-airbyte>=0.23,<0.25",
     ],
-    extras_require={"dev": ["dagit", "pytest"]},
+    extras_require={
+        "dev": ["dagit", "pytest"],
+        "cloud": ["dagster-cloud>=1.6,<1.8"],
+    },
+    include_package_data=True,
+    package_data={
+        "bigstar_data_migration": [
+            "dbt_project/**/*.sql",
+            "dbt_project/**/*.yml",
+        ],
+    },
 )
